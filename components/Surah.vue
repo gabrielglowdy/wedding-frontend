f75a224af58e06de2671b0c30b6d1a9027924f23
Achmad Gabriel Glowdy <39531874+gabrielglowdy@users.noreply.github.com>
2023-01-13 01:40:50 +0700

    Update Surah.vue

--- components/Surah.vue
+++ components/Surah.vue
@@ -1,64 +1,66 @@
 <script setup>
-import { KinesisContainer, KinesisElement } from 'vue-kinesis'
+import { KinesisContainer, KinesisElement } from "vue-kinesis";
 
 const surah = `"Dan di antara tanda-tanda kekuasaan-Nya ialah Dia menciptakan untukmu isteri-isteri dari jenismu sendiri,supaya kamu cenderung dan merasa tenteram kepadanya, dan dijadikan-Nya diantaramu rasa kasih dan sayang.
 Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda bagi kaum yang berfikir."`;
 </script>
 <template>
+  <div class="w-screen">
     <ClientOnly>
-    <KinesisContainer
-      event="scroll"
-      class="max-w-screen h-screen bg-buzz-purple/10 overflow-hidden relative"
-    >
-      <KinesisElement type="depth" class="absolute w-full h-full inset-0">
-        <img
-          src="~assets/img/mosque.jpg"
-          class="
-            hue-rotate-90
-            opacity-20
-            w-full
-            h-full
-            object-cover object-bottom
-          "
-          alt=""
-          srcset=""
-        />
-      </KinesisElement>
-      <div class="relative flex items-center h-full w-full max-w-6xl mx-auto">
-        <div class="flex flex-col mx-8 flex-auto gap-6">
-          <KinesisElement
-            tag="p"
-            :strength="80"
-            type="translate"
+      <KinesisContainer
+        event="scroll"
+        class="w-screen h-screen bg-buzz-purple/10 overflow-hidden relative"
+      >
+        <KinesisElement type="depth" class="absolute w-full h-full inset-0">
+          <img
+            src="~assets/img/mosque.jpg"
             class="
+              hue-rotate-90
+              opacity-20
               w-full
-              text-2xl
-              italic
-              font-dancing-script
-              text-center
-              break-words
-              text-dark/60
+              h-full
+              object-cover object-bottom
             "
-          >
-            {{ surah }}
-          </KinesisElement>
-          <KinesisElement
-            tag="p"
-            :strength="80"
-            type="translate"
-            class="
-              w-full
-              text-4xl
-              font-moon-dance
-              text-center
-              break-words
-              text-buzz-purple
-            "
-          >
-            Ar-Rum Ayat 21
-          </KinesisElement>
+            alt=""
+            srcset=""
+          />
+        </KinesisElement>
+        <div class="relative flex items-center h-full w-full md:max-w-6xl mx-auto">
+          <div class="flex flex-col mx-8 flex-auto gap-6">
+            <KinesisElement
+              tag="p"
+              :strength="80"
+              type="translate"
+              class="
+                w-full
+                text-2xl
+                italic
+                font-dancing-script
+                text-center
+                break-words
+                text-dark/60
+              "
+            >
+              {{ surah }}
+            </KinesisElement>
+            <KinesisElement
+              tag="p"
+              :strength="80"
+              type="translate"
+              class="
+                w-full
+                text-4xl
+                font-moon-dance
+                text-center
+                break-words
+                text-buzz-purple
+              "
+            >
+              Ar-Rum Ayat 21
+            </KinesisElement>
+          </div>
         </div>
-      </div>
-    </KinesisContainer>
+      </KinesisContainer>
     </ClientOnly>
+  </div>
 </template>
