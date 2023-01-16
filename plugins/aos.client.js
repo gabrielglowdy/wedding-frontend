28e6c82f2dbb71f28be22d526e88aaa91cadc489
Achmad Gabriel Glowdy <39531874+gabrielglowdy@users.noreply.github.com>
2023-01-17 04:22:59 +0700

    remove aos offset

--- plugins/aos.client.js
+++ plugins/aos.client.js
@@ -5,7 +5,7 @@ import "aos/dist/aos.css";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.AOS = new AOS.init({
     // disable: window.innerWidth < 640,
-    offset: 200,
+    // offset: 200,
     duration: 600,
     mirror: true,
     easing: 'ease-in-out-cubic',
